542f03784b8e3825c4575a19dafe64b6bd34687c
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2023-01-17 02:17:30 +0530

    lifecycle hook instance start and hostname task fulfilled

---
+++ script.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import subprocess as sp
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('hostname', help='hostname of the instance')
+parser.add_argument('count', help='Number of nfs to be mounted')
+
+args = vars(parser.parse_args(sys.argv[1:]))
+
+hostname=args['hostname']
+_count=args['count']
+
+_,res=sp.getstatusoutput("df -hT")
+res=res.split("\n")
+
+_res=[]
+count=0
+
+for i in res:
+    _res.append(i.split())
+for i in _res:
+    if i[1] == 'nfs4':
+        count+=1
+if count==int(_count):
+    pass
+else:
+    if int(count)-int(_count) == 0:
+        raise Exception("Oops!.... A single EFS is also not found to be mounted. Sorry!! Can't proceed ahead...")
+    elif int(count)-int(_count) == 1:
+        raise Exception("Oops!.... {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...".format(int(count)-int(_count)))
+    else: raise Exception("Oops!.... {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...".format(int(count)-int(_count)))
+_,res=sp.getstatusoutput("hostname")
+if res==hostname:
+    pass
+else:
+    raise Exception("Hostname not matched")
