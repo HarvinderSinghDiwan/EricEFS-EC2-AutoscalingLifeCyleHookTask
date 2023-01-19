cdecf4ccb0b8cdc464dfb0a01fb7af870e070582
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2023-01-20 00:01:12 +0530

    Changed output in a better format as per the client demanded

--- script.py
+++ script.py
@@ -17,7 +17,7 @@ res=res.split("\n")
 
 _res=[]
 count=0
-
+fres=[]
 for i in res:
     _res.append(i.split())
 for i in _res:
@@ -28,17 +28,22 @@ if count==int(_count):
     _r+=1
 else:
     if int(count)-int(_count) == 0:
-        print("Oops!.... A single EFS is also not found to be mounted. Sorry!! Can't proceed ahead...")
+        fres.append("A single EFS is also not found to be mounted.")
         #raise Exception("Oops!.... A single EFS is also not found to be mounted. Sorry!! Can't proceed ahead...")
     elif int(count)-int(_count) == 1:
-        print("Oops!.... {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...")
+        fres.append("{} EFS seems to be missing or unmounted.".format(count-_count))
         #raise Exception("Oops!.... {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...".format(int(count)-int(_count)))
-    else: print("Oops!.... {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...")
+    else: fres.append(" {} EFS seems to be missing or unmounted. Sorry!! Can't proceed ahead...".format(count-_count))
 _,res=sp.getstatusoutput("hostname")
 if res==hostname:
     _r+=1
 else:
     #raise Exception("Hostname not matched")
-    print("HOSTNAME NOT FOUND")
+    fres.append("Current hostname of the instance doesn't seems to match with the desired hostname provided where CURRENT_HOSTNAME is {} and DESIRED_HOSTNAME IS {}".format(res,hostname))
 if _r==2:
     print(0)
+else:
+    if len(fres) is 2:
+        print(fres[0]+" and "+fres[1])
+    else: print(fres[0])
+
